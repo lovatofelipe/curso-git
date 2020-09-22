fefd12a15478568895d95efa6ae92f329ac2afcc
Felipe Lovato <lovato.flp@gmail.com>
2020-09-21 23:11:38 -0300

    Curso de C#, teste git

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                String[] value = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(value[j]);
+                }
+                
+            }
+
+        }
+    }
+}
+
