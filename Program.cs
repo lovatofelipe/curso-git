bfd4fb6c327126efb1d98cdb6d6c803a77156486
Felipe Lovato <lovato.flp@gmail.com>
2020-09-21 23:13:42 -0300

    modificado para teste

--- Program.cs
+++ Program.cs
@@ -8,17 +8,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[,] mat = new int[n, n];
-            for (int i = 0; i < n; i++)
-            {
-                String[] value = Console.ReadLine().Split(' ');
-                for (int j = 0; j < n; j++)
-                {
-                    mat[i, j] = int.Parse(value[j]);
-                }
-                
-            }
+            Console.WriteLine("teste de versões");
 
         }
     }
